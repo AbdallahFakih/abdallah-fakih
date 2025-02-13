4f924b31f1ea658ba839292007c62fff4d4b3637
AbdallahFakih <aboudifakih93@gmail.com>
2025-02-13 23:18:01 +0200

    test commit

--- example.py
+++ example.py
@@ -1,2 +1,4 @@
+print ("my name is aboudi fakih and i am a student at the university of balamand in lebanon and i am studying computer science and i am in my second year and i am 20 years old and i am from lebanon and i am from the city of tripoli and i am a big fan of programming and i am a big fan of python and i am a big fan of the university of balamand and i am a big fan of the city of tripoli and i am a big fan of lebanon and i am a big fan of computer science and i am a big fan of the internet and i am a big fan of technology and i am a big fan of the world and i am a big fan of the universe and i am a big fan of the galaxy and i am a big fan of the solar system and i am a big fan of the planet earth and i am a big fan of the human")
 for i in range(1,11):
         print(f"number:{i}")
+        
